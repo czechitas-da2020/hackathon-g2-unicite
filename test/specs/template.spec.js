11ac02b8a6780e05e3116ba81bbea48fbced0811
Tereza Pospichalova <tereza.pospichalova@gmail.com>
2020-11-14 17:44:55 +0100

    upraveny kod

--- test/specs/template.spec.js
+++ test/specs/template.spec.js
@@ -1,7 +1,17 @@
 const templatePage = require('../pages/template.page')
 
 describe('Template suite', () => {
+
+    before(() => {
+        browser.url('http://czechitas-shopizer.azurewebsites.net/shop/')
+    });
+
     it('template test-case', () => {
-        
+        browser.$('.addToCart[productid="1"]').click()
+        browser.pause(3000)
+
+        browser.url('http://czechitas-shopizer.azurewebsites.net/shop/cart/shoppingCart.html')
+        expect(browser($('h1'))).toHaveText('Review your order')
+        expect($('.nomargin')).toHaveText('Retro style bag')
     })
 })
