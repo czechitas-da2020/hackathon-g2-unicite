adaa60b27c4556dbcdb7be6208dad5d5ffeac90a
Eliska Svecova <svecova.elliska@gmail.com>
2020-11-21 18:28:41 +0100

    inovace kodu ve slozce uvod.js

--- test/uvod.js
+++ test/uvod.js
@@ -1,21 +1,25 @@
 describe('Web Three foxes Lounge', () => {
+
     it('overim nacteni stranky', () => {
         browser.url('http://www.czechitas-hackhaton.cz/en/');
-        let nadpis = $('h1')
-        console.log('Nadpis je: ' + nadpis.getText())
+        let nadpis = $('h1');
+        console.log('Nadpis je: ' + nadpis.getText());
     
         //overeni 
         expect(nadpis).toHaveTextContaining('foxes')
     });
 
 
-    it('kviknu na Sign in', () => {
-        let buttonSignIn = $('span.hide_xs')
-        console.log('Button ma nazev: ' + buttonSignIn.getText())
-        browser.$(buttonSignIn).click()
-        browser.pause(3000)
-    
+    it('napsani emailu do newsletteru', () => {
+        let buttonNewsLetter = $('#newsletter-input');
+        let buttonSubscribe = $('[name="submitNewsletter"]');
+        browser.$(buttonNewsLetter).scrollIntoView();
+        browser.$(buttonNewsLetter).setValue('amail@gmail.com');
+        browser.$(buttonSubscribe).click();
+        browser.pause(5000);
+
         //overeni 
-        expect(buttonSignIn).toHaveAttribute('url','http://www.czechitas-hackhaton.cz/en/login?back=my-account')
-    });
+        expect($('#newsletter-input')).toHaveValueContaining('A verification email has been sent.');
+        browser.saveScreenshot('./newsletter.png');
+    })
 })    
